504cc5b315070ddd5269e8e9eeb2496d568afeba
W-34 <20281022@bjtu.edu.cn>
2023-04-05 21:16:17 +0800

    modified:   3.2.2/src/Sort.java

--- 3.2.2/src/Sort.java
+++ 3.2.2/src/Sort.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.io.IntWritable;
 public class Sort {
